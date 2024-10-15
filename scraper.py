807de387b138f9962ae566334e7431cf8527f541
AhmerRaza12 <ahmer183gamerz@gmail.com>
2024-10-15 14:03:31 +0500

    final changes

--- scraper.py
+++ scraper.py
@@ -50,7 +50,7 @@ chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_argument('--start-maximized')
 chrome_options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/128.0.0.0 Safari/537.36')
 chrome_options.add_argument('--ignore-certificate-errors')
-# chrome_options.add_argument(f'--proxy-server={proxy_ip}')
+chrome_options.add_argument(f'--proxy-server={proxy_ip}')
 # chrome_options.add_argument('--window-size=1920,1080')
 chrome_install = ChromeDriverManager().install()
 folder = os.path.dirname(chrome_install)
@@ -129,7 +129,7 @@ def get_profile_links():
     
     login_now_button = driver.find_element(By.XPATH, "//div[@id='login-button']")
     login_now_button.click()
-    time.sleep(10)
+    time.sleep(15)
     
     advanced_search = driver.find_element(By.XPATH, "//div[@data-tour-step='buyer-supplier-search']")
     advanced_search.click()
@@ -180,139 +180,181 @@ def get_profile_links():
     driver.quit()
 
 # read links from profile_links.txt each line is a link
-profile_links = open("profile_links.txt", "r").read().split("\n")
+count_file_path = "count.txt"
+
+def get_last_scraped_count():
+    if os.path.exists(count_file_path):
+        with open(count_file_path, "r") as f:
+            count = f.read().strip()
+            return int(count) if count.isdigit() else 0
+    return 0
+
+def update_scraped_count(current_count):
+    with open(count_file_path, "w") as f:
+        f.write(str(current_count))
+
+profile_links = open("remaining_links.txt", "r").read().split("\n")
 
 all_data = []
 def get_data(profiles_links):
-        for profile in profiles_links[:5]:
-            driver.get(profile)
-            if profile == profiles_links[0]:
+    last_scraped = get_last_scraped_count()
+    for index, profile in enumerate(profiles_links[last_scraped:], start=last_scraped+1):
+        driver.get(profile)
+        try:
+            cookie_button = driver.find_element(By.XPATH, "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']")
+            if cookie_button:
                 try:
                     chatbox_thread = threading.Thread(target=close_chatbox, daemon=True)
                     chatbox_thread.start()
                     try:
-                        cookie_button = driver.find_element(By.XPATH, "//button[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']")
+                    
                         cookie_button.click()
                         time.sleep(1)
                     except:
                         pass
-                    menu=driver.find_element(By.XPATH, "//div[@id='menueNavBtn']")
+                    menu = driver.find_element(By.XPATH, "//div[@id='menueNavBtn']")
                     menu.click()
                     time.sleep(1)
-                    login_button=driver.find_element(By.XPATH, "(//div[@data-refid='loginButton'])[2]")
+                    login_button = driver.find_element(By.XPATH, "(//div[@data-refid='loginButton'])[2]")
                     login_button.click()
                     time.sleep(5)
-                    username_input=driver.find_element(By.XPATH, "//input[@id='logname']")
-                    password_input=driver.find_element(By.XPATH, "//input[@id='password']")
+                    username_input = driver.find_element(By.XPATH, "//input[@id='logname']")
+                    password_input = driver.find_element(By.XPATH, "//input[@id='password']")
                     for char in techpilot_username:
                         username_input.send_keys(char)
                         time.sleep(0.1)
                     for char in techpilot_password:
                         password_input.send_keys(char)
                         time.sleep(0.1)
-                    login_now_button=driver.find_element(By.XPATH, "//div[@id='login-button']")
+                    login_now_button = driver.find_element(By.XPATH, "//div[@id='login-button']")
                     login_now_button.click()
-                    time.sleep(8)
-                    # now we need to go to the profile link
+                    time.sleep(10)
                     driver.get(profile)
-                    time.sleep(3)
+                    time.sleep(1)
                 except Exception as e:
                     print("Error while login")
                     print(e)
-            time.sleep(3)
-            show_phone_numbers= driver.find_elements(By.XPATH, "//span[@class='phonePlaceholder']")
-            for show_phone_number in show_phone_numbers:
+        except Exception as e:
+            pass
+        time.sleep(1)
+        show_phone_numbers = driver.find_elements(By.XPATH, "//span[@class='phonePlaceholder']")
+        for show_phone_number in show_phone_numbers:
+            try:
                 show_phone_number.click()
                 time.sleep(1)
-            try:    
-                company_name=driver.find_element(By.XPATH, "//h1").text
-            except:
-                company_name=""
-            try:
-                company_addr=driver.find_element(By.XPATH, "//div[@id='labelAddress']/ul").text
-            except:
-                company_addr=""
-            try:
-                company_contact=driver.find_element(By.XPATH, "//div[@id='labelAddress']/ul").text
-            except:
-                company_contact=""
-            try:
-                                company_data=driver.find_element(By.XPATH, "(//div[@id='labelCompData']/ul)[2]").text
-            except:
-                company_data=""
-            try:
-                company_location=driver.find_element(By.XPATH, "//div[@id='locationMap']/a").get_attribute("href")
-            except:
-                company_location=""
-            try:
-                other_locations=driver.find_element(By.XPATH, "//div[@id='furtherLocations']").text
-            except:
-                other_locations=""
-            file_downloads,technologies=[],[]
-            try:
-                download_files=driver.find_elements(By.XPATH, "//div[@id='documentDownloads']//a")
-                for download_file in download_files:
-                    download_file_href=download_file.get_attribute("href")
-                    file_downloads.append(download_file_href)
             except:
                 pass
-    
-            try:
-                company_contact_persons=driver.find_element(By.XPATH, "//div[@id='perfectContactPerson']").text
-            except:
-                company_contact_persons=""
-            try:
-                other_technologies_readmore=driver.find_element(By.XPATH, "//div[@id='perfectTechnology']/div[@class='readMore']")
-                other_technologies_readmore.click()
-                time.sleep(1)
-                other_technologies=driver.find_elements(By.XPATH, "//div[@id='perfectTechnology']//h3")
-                for other_technologie in other_technologies:
-                    technologies.append(other_technologie.text)
-            except:
-                pass
-            try:
-                company_materials=driver.find_element(By.XPATH, "//div[@id='perfectMaterials']").text
-            except:
-                company_materials=""
-            try:
-                company_industries=driver.find_element(By.XPATH, "//div[@id='perfectIndustry']").text
-            except:
-                company_industries=""
-            try:
-                company_references=driver.find_element(By.XPATH, "//div[@id='perfectReferences']").text
-            except:
-                company_references=""
-            machines_data=[]
-            try:
-                machines_data_elements = driver.find_elements(By.XPATH, "//div[@class='rssTpSliderBox']")
-                for machine_data_element in machines_data_elements:
-                    company_machine = driver.execute_script("return arguments[0].textContent", machine_data_element)
-                    cleaned_machine = company_machine.replace("\n", " ").replace("\t", "").strip()
-                    if '.jpg' not in cleaned_machine:
-                        machines_data.append(cleaned_machine)
-            except:
-                pass
-            data={
-                "Company Name":company_name,
-                "Company Address":company_addr,
-                "Company Contact":company_contact,
-                "Company Data":company_data,
-                "Company Location":company_location,
-                "Other Locations":other_locations,
-                "File Download Links":"\n".join(file_downloads),
-                "Company Contact Persons":company_contact_persons,
-                "Technologies used":"\n".join(technologies),
-                "In use Materials":company_materials,
-                "Industries":company_industries,
-                "References":company_references,
-                "Machines":"\n".join(machines_data)
-            }
-            print(data)
-            all_data.append(data)
-            appendProduct("techpilot_data.csv",data)
+        try:
+            company_name = driver.find_element(By.XPATH, "//h1").text
+        except:
+            company_name = ""
+        try:
+            company_addr = driver.find_element(By.XPATH, "//div[@id='labelAddress']/ul").text
+        except:
+            company_addr = ""
+        try:
+            company_contact = driver.find_element(By.XPATH, "//div[@id='labelContact']/ul").text
+        except:
+            company_contact = ""
+        try:
+            emp_range = driver.find_element(By.XPATH, "//div[@id='labelCompData']/ul/li[2]/span[2]")
+            emp_range_text= driver.execute_script("return arguments[0].textContent", emp_range)
+            emp_range_text = emp_range_text.replace("\n", "").replace("\t", "").strip()
+        except:
+            emp_range_text = ""
+        try:
+            founding_year = driver.find_element(By.XPATH, "//div[@id='labelCompData']/ul/li[3]/span[2]")
+            founding_year_text = driver.execute_script("return arguments[0].textContent", founding_year)
+            founding_year_text = founding_year_text.replace("\n", "").replace("\t", "").strip()
+
+        except:
+            founding_year_text = ""
+        try:
+            revenue_range = driver.find_element(By.XPATH, "//div[@id='labelCompData']/ul/li[4]/span[2]")
+            revenue_range_text = driver.execute_script("return arguments[0].textContent", revenue_range)
+            revenue_range_text = revenue_range_text.replace("\n", "").replace("\t", "").strip()
+        except:
+            revenue_range_text = ""
+        # try:
+        #     company_data = driver.find_element(By.XPATH, "(//div[@id='labelCompData']/ul)[2]").text
+        # except:
+        #     company_data = ""
+        try:
+            company_location = driver.find_element(By.XPATH, "//div[@id='locationMap']/a").get_attribute("href")
+        except:
+            company_location = ""
+        try:
+            other_locations = driver.find_element(By.XPATH, "//div[@id='furtherLocations']").text
+        except:
+            other_locations = ""
+        file_downloads, technologies = [], []
+        try:
+            download_files = driver.find_elements(By.XPATH, "//div[@id='documentDownloads']//a")
+            for download_file in download_files:
+                download_file_href = download_file.get_attribute("href")
+                file_downloads.append(download_file_href)
+        except:
+            pass
+        try:
+            company_contact_persons = driver.find_element(By.XPATH, "//div[@id='perfectContactPerson']").text
+        except:
+            company_contact_persons = ""
+        try:
+            other_technologies_readmore = driver.find_element(By.XPATH, "//div[@id='perfectTechnology']/div[@class='readMore']")
+            other_technologies_readmore.click()
+            time.sleep(1)
+            other_technologies = driver.find_elements(By.XPATH, "//div[@id='perfectTechnology']//h3")
+            for other_technologie in other_technologies:
+                technologies.append(other_technologie.text)
+        except:
+            pass
+        try:
+            company_materials = driver.find_element(By.XPATH, "//div[@id='perfectMaterials']").text
+        except:
+            company_materials = ""
+        try:
+            company_industries = driver.find_element(By.XPATH, "//div[@id='perfectIndustry']").text
+        except:
+            company_industries = ""
+        try:
+            company_references = driver.find_element(By.XPATH, "//div[@id='perfectReferences']").text
+        except:
+            company_references = ""
+        machines_data = []
+        try:
+            machines_data_elements = driver.find_elements(By.XPATH, "//div[@class='rssTpSliderBox']")
+            for machine_data_element in machines_data_elements:
+                company_machine = driver.execute_script("return arguments[0].textContent", machine_data_element)
+                cleaned_machine = company_machine.replace("\n", " ").replace("\t", "").strip()
+                if '.jpg' and '.JPG' not in cleaned_machine:
+                    machines_data.append(cleaned_machine)
+        except:
+            pass
+        data = {
+            "Company Name": company_name,
+            "Company Address": company_addr,
+            "Company Contact": company_contact,
+            "Employee Range": emp_range_text,
+            "Founding Year": founding_year_text,
+            "Revenue Range": revenue_range_text,
+            "Company Location": company_location,
+            "Other Locations": other_locations,
+            "File Download Links": "\n".join(file_downloads),
+            "Company Contact Persons": company_contact_persons,
+            "Technologies used": "\n".join(technologies),
+            "In use Materials": company_materials,
+            "Industries": company_industries,
+            "References": company_references,
             
-        driver.quit()
-        return all_data
+            "Machines ": "\n".join([f"{index + 1}. {machine}" for index, machine in enumerate(machines_data)])
+        }
+        print(data)
+        all_data.append(data)
+        appendProduct("techpilot_data_rem.csv", data)
+        update_scraped_count(index)
+        
+    driver.quit()
+    return all_data
 
 profiles_scraped = get_data(profile_links)
 print(f"Scraped {len(profiles_scraped)} profiles.")
